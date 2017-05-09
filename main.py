f03d28d2c6274f50a901dae7d1946dae0cac0432
samling <samlingx@gmail.com>
2017-05-08 20:26:46 -0700

    Adding basic ajax templating

--- main.py
+++ main.py
@@ -13,6 +13,26 @@ db = nfldb.connect()
 def index():
     return render_template('index.html')
 
+@app.route("/_render_player_info")
+def render_player_info():
+    name = request.args.get('name')
+    birthdate = request.args.get('birthdate')
+    college = request.args.get('college')
+    profile_url = request.args.get('profile_url')
+    team = request.args.get('team')
+    uniform_number = request.args.get('uniform_number')
+    weight = request.args.get('weight')
+    years_pro = request.args.get('years_pro')
+    return render_template('player_info.html',
+            name=name,
+            birthdate=birthdate,
+            college=college,
+            profile_url=profile_url,
+            team=team,
+            uniform_number=uniform_number,
+            weight=weight,
+            years_pro=years_pro)
+
 @app.route("/_search_by_year/<season_year>/<season_type>/<season_week>")
 def search_year(season_year, season_type, season_week):
     q = nfldb.Query(db)
@@ -25,8 +45,22 @@ def search_year(season_year, season_type, season_week):
     games = q.as_players()
     player_list = []
     for player in games:
-        if player.full_name.lower().startswith(name) or player.last_name.lower().startswith(name):
-            data = {'label': player.full_name, 'value': player.full_name, 'id': player.player_id}
+        print dir(player)
+        if player.first_name.lower().startswith(name) or player.last_name.lower().startswith(name):
+            data = {
+                    'label': player.full_name,
+                    'value': player.full_name,
+                    'id': player.player_id,
+                    'first_name': player.first_name,
+                    'last_name': player.last_name,
+                    'birthdate': player.birthdate,
+                    'college': player.college,
+                    'profile_url': player.profile_url,
+                    'team': player.team,
+                    'uniform_number': player.uniform_number,
+                    'weight': player.weight,
+                    'years_pro': player.years_pro
+                    }
             player_list.append(data)
     return jsonify(matching_results=player_list)
 
