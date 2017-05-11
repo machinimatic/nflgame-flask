1f8030f0436331e26baf6edd27d67876369deff2
samling <samlingx@gmail.com>
2017-05-11 12:05:39 -0700

    Rebuilding virtualenv

--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sboynton/Documents/Programming/Python/nflgame"
+set -gx VIRTUAL_ENV "/Users/sboynton/Documents/Programming/Python/nflgame-flask"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
