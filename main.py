4a46f451085fa171a74417a69c219f2406a4afae
samling <samlingx@gmail.com>
2017-05-10 22:57:39 -0700

    Mostly working search form, still not getting postseason and preseason values by game for some reason

--- main.py
+++ main.py
@@ -4,26 +4,30 @@ import nfldb
 import requests
 from stat_names import stat_names
 import sys
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask import render_template
 app = Flask(__name__)
+app.config.from_envvar('NFLGAME_APP_SETTINGS')
 
 db = nfldb.connect()
 
 @app.route("/")
 def index():
-    return render_template('header.html')
+    return render_template('index.html')
 
 @app.route("/player/<player_id>")
 @app.route("/player/<player_id>/<int:year>")
-@app.route("/player/<player_id>/<int:year>/<int:week>")
-def render_player_info(player_id, year=None, week=None):
+@app.route("/player/<player_id>/<int:year>/<phase>")
+@app.route("/player/<player_id>/<int:year>/<phase>/<int:week>")
+def render_player_info(player_id, year="All", phase="All", week="All"):
     player = nfldb.Player.from_id(db, player_id)
     q = nfldb.Query(db).game()
 
-    if 2009 <= year <= 2016:
+    if (phase == "Preseason") or (phase == "Regular") or (phase == "Postseason"):
+        q.game(season_type=phase)
+    if 2009 <= int(year) <= 2016:
         q.game(season_year=year)
-    if 1 <= week <= 17:
+    if 1 <= int(week) <= 17:
         q.game(week=week)
 
     q.player(gsis_name=player.gsis_name, team=player.team)
@@ -35,7 +39,13 @@ def render_player_info(player_id, year=None, week=None):
             # Reverse this so it joins properly with category list
             stat_list[field] = int(getattr(stat, field))
 
-    return render_template('player.html', player=player, stats=stat_list, stat_names=stat_names)
+    session['prev_year'] = year
+    session['prev_phase'] = phase
+    session['prev_week'] = week
+    session['prev_player_id'] = str(player.player_id)
+    session['prev_player_name'] = str(player.full_name)
+
+    return render_template('player.html', player=player, stats=stat_list, stat_names=stat_names, session=session)
 
 @app.route("/_render_player_stats/<player_id>")
 def render_player_stats(player_id):
