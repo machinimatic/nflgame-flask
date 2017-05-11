784447949ef0bca3f2d0e1590187ea419692f0a6
samling <samlingx@gmail.com>
2017-05-10 19:52:25 -0700

    Major revision to layout, separating out some macros and definitions, improving routes, simplifying logic

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import json
 import nflgame
 import nfldb
-import pprint
 import requests
+from stat_names import stat_names
 import sys
 from flask import Flask, request, jsonify
 from flask import render_template
@@ -12,22 +12,30 @@ db = nfldb.connect()
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route("/player/<player_id>")
-def render_player_info(player_id):
+@app.route("/player/<player_id>/<int:year>")
+@app.route("/player/<player_id>/<int:year>/<int:week>")
+def render_player_info(player_id, year=None, week=None):
     player = nfldb.Player.from_id(db, player_id)
-
     q = nfldb.Query(db).game()
+
+    if 2009 <= year <= 2016:
+        q.game(season_year=year)
+    if 1 <= week <= 17:
+        q.game(week=week)
+
     q.player(gsis_name=player.gsis_name, team=player.team)
     stats = q.as_aggregate()
 
     stat_list = {}
     for stat in stats:
         for field in stat.fields:
-            stat_list[field] = getattr(stat, field)
+            # Reverse this so it joins properly with category list
+            stat_list[field] = int(getattr(stat, field))
 
-    return render_template('player.html', player=player, stats=stat_list)
+    return render_template('player.html', player=player, stats=stat_list, stat_names=stat_names)
 
 @app.route("/_render_player_stats/<player_id>")
 def render_player_stats(player_id):
